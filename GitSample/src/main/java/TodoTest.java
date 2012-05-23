c6ed5813130dc3195d7ab99f97b83e12d199d579
kaakaa <stooner.hot@gamil.com>
2012-05-23 21:38:58 +0900

    gradle

---
+++ GitSample/src/main/java/TodoTest.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class TodoTest {
+
+	private String string;
+	
+	private int number;
+	
+	public TodoTest(String string, int number){
+		this.string = string;
+		this.number = number;
+	}
+	
+	// TODO Under Construction
+	public void test(){
+		
+	}
+	
+	// TODO UnderConstruction2
+	public void test2(){
+		
+	}
+	// FIXME UnderCOnstruction3
+}
